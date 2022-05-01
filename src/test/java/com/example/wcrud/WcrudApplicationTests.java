05da8724094db0f2d147764276856f5347e8402e
Guilherme <ferreiragui.dev@gmail.com>
2022-05-01 13:03:35 -0300

    W.CRUD

---
+++ src/test/java/com/example/wcrud/WcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
