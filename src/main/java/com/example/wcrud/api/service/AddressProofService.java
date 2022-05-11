40329ce06e49be98db2fd7223b46c9b753c45ce1
Guilherme <ferreiragui.dev@gmail.com>
2022-05-11 17:07:56 -0300

    Treatment to replace existent image added

--- src/main/java/com/example/wcrud/api/service/AddressProofService.java
+++ src/main/java/com/example/wcrud/api/service/AddressProofService.java
@@ -2,12 +2,17 @@ package com.example.wcrud.api.service;
 
 import com.example.wcrud.api.model.AddressProof;
 import com.example.wcrud.api.repository.AddressProofRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
 
+import static org.apache.commons.io.FileUtils.forceDelete;
+
+@Slf4j
 @Service
 public class AddressProofService {
 
@@ -17,7 +22,7 @@ public class AddressProofService {
 
     private static final String PATH = "./address-proof/";
 
-    public AddressProofService(ImageService imageService, AddressProofRepository addressProofRepository,AddressService addressService) {
+    public AddressProofService(ImageService imageService, AddressProofRepository addressProofRepository, AddressService addressService) {
         this.imageService = imageService;
         this.addressProofRepository = addressProofRepository;
         this.addressService = addressService;
@@ -25,9 +30,22 @@ public class AddressProofService {
     }
 
     public AddressProof save(final MultipartFile file, final Long id) throws IOException {
+        var addressProof = addressService.findById(id).getAddressProof();
+
+        if (addressProof != null) {
+            try {
+                forceDelete(new File(addressProof.getPath()));
+                log.info("Image was successfully overrode");
+
+            } catch (Exception e) {
+                log.info("ERROR: COULD NOT DELETE");
+            }
+        } else {
+            addressProof = new AddressProof();
+        }
+
         String path = imageService.saveImage(file, PATH);
 
-        AddressProof addressProof = new AddressProof();
         addressProof.setPath(path);
         addressProof.setCreatedAt(LocalDateTime.now());
         addressProof.setAddress(addressService.findById(id));
